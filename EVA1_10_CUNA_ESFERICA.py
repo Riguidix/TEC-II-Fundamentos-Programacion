afad09260d423bf2a1a0d2c2fa9a951182789ad9
Riguidix <reydi1@hotmail.com>
2019-02-02 00:38:50 -0700

    Update EVA1_10_CUNA_ESFERICA.py

--- EVA1_10_CUNA_ESFERICA.py
+++ EVA1_10_CUNA_ESFERICA.py
@@ -1,22 +1,4 @@
 import numpy
-"""
-        // TODO code application logic here
-        // ( (4 / 3) * Pi * radio ^ 3 * nGrados ) / 360
-        final double PI = Math.PI;
-        float radio;
-        double grados;
-        double volumen;
-            
-        Scanner input = new Scanner(System.in);
-        
-        System.out.println("Introduce el valor del radio");
-        radio = input.nextFloat();
-        System.out.println("Introduce el valor de los grados");
-        grados = input.nextFloat();
-        
-        volumen = ( (4/3) * PI * (Math.pow(radio, 3)) * grados ) / 360;
-        System.out.println("El valor del volumen es: " + volumen);
-"""
 radio = 0
 grados = 0
 volumen = 0
